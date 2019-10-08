eac63c295e037b704a56fcf28bdf3cff5e06e864
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-08 21:51:05 +0900

    basic auth fix unicorn add
    ベーシック認証の環境変数設定と、Basic認証を本番環境でのみ行う記述を行った。
    また、unicorn.rbを作成し、必要な記述を行った。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :basic_auth
+  before_action :basic_auth, if: :production?
 
   private
+
+  def production?
+    Rails.env.production?
+  end
 
   #Basic認証を要求するための記述
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'weekend56b' && password == '1005b'
+      username ==  ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
   end
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,16 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.2"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, 'freemarket_sample_56b'
+set :repo_url, 'git@github.com:furuyaatsushi/freemarket_sample_56b.git'
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :rbenv_type, :user
+set :rbenv_ruby, '2.5.1'
+set :ssh_options, auth_methods: ['publickey'],
+                  keys: ['~/.ssh/Atsushi_Key.pem']
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -37,3 +45,10 @@ set :repo_url, "git@example.com:me/my_repo.git"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
