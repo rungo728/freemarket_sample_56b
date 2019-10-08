4f4041d859227a1c7c9ba9aaf4f7c48d4a20e6a4
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-08 18:58:36 +0900

    add basic auth
    Basic認証を導入するための記述を行いました。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :basic_auth
+
+  private
+
+  #Basic認証を要求するための記述
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'weekend56b' && password == '1005b'
+    end
+  end
 end
