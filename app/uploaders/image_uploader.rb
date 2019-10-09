00999434f241221570b1ab5f29ea94b9c4a69e6c
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-09 23:43:31 +0900

    image upload file
    画像のアップロード先を指定する記述をした。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,8 +4,13 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  
+  #開発環境と本番環境での画像アップロード先の条件分岐
+  if Rails.env.development? || Rails.env.test? 
+    storage :file
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
