14095743a6990f47895cfdf11ffcdfebd42f118e
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-13 01:18:47 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,8 +8,6 @@
 # server "db.example.com", user: "deploy", roles: %w{db}
 server '3.113.193.0', user: 'ec2-user', roles: %w{app db web}
 
-set :rails_env, "production"
-set :unicorn_rack_env, "production"
 
 
 # role-based syntax
