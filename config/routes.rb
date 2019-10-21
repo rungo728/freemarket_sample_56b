1fa6908d71c7f2385c675802ad31e2eba63a89f9
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-21 21:17:13 +0900

    small fix
    小さな修正を行った

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'tests', to: 'tests#show'
+  get 'tests', to: 'tests#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
