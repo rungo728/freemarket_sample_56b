026820ce962672e70b65e526c38a20dbe10f1277
oike-github <chikakoike.work@gmail.com>
2019-10-26 14:35:15 +0900

    add-devise-user
    Added devise user model and views.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191026052020) do
+ActiveRecord::Schema.define(version: 20191026052650) do
 
   create_table "prefectures", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -18,4 +18,33 @@ ActiveRecord::Schema.define(version: 20191026052020) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "nickname",                                          null: false
+    t.string   "email",                                default: "", null: false
+    t.string   "encrypted_password",                   default: "", null: false
+    t.string   "last_name",                                         null: false
+    t.string   "first_name",                                        null: false
+    t.string   "last_name_kana",                                    null: false
+    t.string   "first_name_kana",                                   null: false
+    t.date     "birthday",                                          null: false
+    t.string   "phone_number",                                      null: false
+    t.string   "postcode",                                          null: false
+    t.integer  "prefecture_id",                                     null: false
+    t.string   "city",                                              null: false
+    t.string   "address",                                           null: false
+    t.string   "building"
+    t.string   "phone_number_sub"
+    t.text     "profile",                limit: 65535
+    t.integer  "point",                                             null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["prefecture_id"], name: "index_users_on_prefecture_id", using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  add_foreign_key "users", "prefectures"
 end
