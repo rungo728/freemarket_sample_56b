8610ff60a62996491b934f2dcc8d8a178cbb8db8
furuyaatsushi <sdvxmnn@gmail.com>
2019-10-27 16:58:07 +0900

    create brand table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191027074923) do
+ActiveRecord::Schema.define(version: 20191027075125) do
+
+  create_table "brands", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name",        null: false
+    t.integer  "category_id", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["category_id"], name: "index_brands_on_category_id", using: :btree
+  end
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",       null: false
@@ -73,6 +81,7 @@ ActiveRecord::Schema.define(version: 20191027074923) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "brands", "categories"
   add_foreign_key "credits", "users"
   add_foreign_key "evaluations", "users"
   add_foreign_key "users", "prefectures"
