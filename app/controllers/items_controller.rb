55bd7df3f1d22af28c8e3284635bb6fc5c1197be
furuyaatsushi <sdvxmnn@gmail.com>
2019-11-03 12:37:49 +0900

    mens list
    メンズの商品を並べる記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def index
     @ladies = Item.includes(:photos).where(category_id: 0..199).order("id DESC").limit(10)
+    @mens = Item.includes(:photos).where(category_id: 200..345).order("id DESC").limit(10)
   end
 
   def confirmation
