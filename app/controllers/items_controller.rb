780967938882499c756cbbdbb0ad20146187b221
furuyaatsushi <sdvxmnn@gmail.com>
2019-11-03 16:26:10 +0900

    chanels list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,12 @@
 class ItemsController < ApplicationController
   def index
+    #カテゴリーごとに並べる
     @ladies = Item.includes(:photos).where(category_id: 0..199).order("id DESC").limit(10)
     @mens = Item.includes(:photos).where(category_id: 200..345).order("id DESC").limit(10)
     @phones = Item.includes(:photos).where(category_id: 898..983).order("id DESC").limit(10)
     @toys = Item.includes(:photos).where(category_id: 685..797).order("id DESC").limit(10)
+    #ブランドごとに並べる
+    @chanels = Item.includes(:photos).where(brand_id: 0).order("id DESC").limit(10)
   end
 
   def confirmation
