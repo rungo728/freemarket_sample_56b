e40a51abb76aac4595a3f9a694636a5791af86e3
furuyaatsushi <sdvxmnn@gmail.com>
2019-11-03 13:45:42 +0900

    phone list

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   def index
     @ladies = Item.includes(:photos).where(category_id: 0..199).order("id DESC").limit(10)
     @mens = Item.includes(:photos).where(category_id: 200..345).order("id DESC").limit(10)
+    @phones = Item.includes(:photos).where(category_id: 898..983).order("id DESC").limit(10)
   end
 
   def confirmation
