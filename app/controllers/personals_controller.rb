6937f6e8bd9e461dd1b46e84f74b66b6aeae31e8
furuyaatsushi <sdvxmnn@gmail.com>
2019-12-14 16:00:06 +0900

    personal-edit
    本人情報を持ってきて表示するところまで実装

--- app/controllers/personals_controller.rb
+++ app/controllers/personals_controller.rb
@@ -7,12 +7,24 @@ class PersonalsController < ApplicationController
   end
 
   def update
+    @personal.update(personal_params)
+
+    if @personal.save
+      redirect_to edit_personal_path, notice: '更新が完了しました'
+    else
+      render 'edit'
+    end
   end
 
   private
 
   def get_personal
     @personal = Personal.find_by(user_id: current_user.id)
+  end
+
+  def personal_params
+    binding.pry
+    params.require(:personal).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :birthday, :postcord, :prefecture_id, :city, :address, :building)
   end
 
   def set_search
