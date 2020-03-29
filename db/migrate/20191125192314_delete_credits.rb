ebdcd92c3540f21ce934fd11f811bfceb3bde120
rungo728 <rufii72865@gmail.com>
2020-03-29 23:48:19 +0900

    delete migrate file

--- db/migrate/20191125192314_delete_credits.rb
+++ db/migrate/20191125192314_delete_credits.rb
@@ -1,5 +1,5 @@
-class DeleteCredits < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :credits
-  end
-end
+# class DeleteCredits < ActiveRecord::Migration[5.0]
+#   def change
+#     drop_table :credits
+#   end
+# end
