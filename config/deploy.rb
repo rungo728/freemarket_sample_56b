2861767afdff95c29d6df327426930860eb1ddc4
rungo728 <rufii72865@gmail.com>
2020-03-30 20:34:16 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,12 +2,12 @@
 lock "3.11.2"
 
 set :application, 'freemarket_sample_56b'
-set :repo_url, 'git@github.com:furuyaatsushi/freemarket_sample_56b.git'
+set :repo_url, 'git@github.com:rungo728/freemarket_sample_56b.git'
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/56b.pem']
+                  keys: ['~/.ssh/KK.pem']
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
