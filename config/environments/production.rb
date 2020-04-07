13b64b1e7e3a1dbf2e5be4aa7115cd35ae6e586d
rungo728 <rufii72865@gmail.com>
2020-04-08 08:56:29 +0900

    smail fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,5 +84,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
+  secret_key_base: ENV["SECRET_KEY_BASE"]
 end
